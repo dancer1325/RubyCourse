1079ec5bb37048e1256c6b7cb730269650498fbf
dancer1325 <alfredotic0809@gmail.com>
2022-02-20 23:40:48 +0100

    Fix naming for getting each entry. Add tens

--- FirstChapter/exercise_1.rb
+++ FirstChapter/exercise_1.rb
@@ -10,10 +10,12 @@ puts "Hello #{name} #{last_name}"
 print 'Please, introduce a number of 4 digits: '
 number = gets.to_i   # chomp Returns a new string.
 
-thousands = number % 1000 / 100
-hundreds = number % 1000 % 100 / 10
+thousands = number % 1000
+hundreds = number % 1000 / 100
+tens = number % 1000 % 100 / 10
 units = number % 1000 % 100 % 10
 
 puts "thousands #{thousands}"
 puts "hundreds #{hundreds}"
+puts "tens #{tens}"
 puts "units #{units}"
