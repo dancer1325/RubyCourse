ca1cad9aa7de491ee892b20f2ed4c7173f03d34c
dancer1325 <alfredotic0809@gmail.com>
2022-02-20 18:11:46 +0100

    Add commentaries. Fix a problem

--- FirstChapter/StringAndSymbols.rb
+++ FirstChapter/StringAndSymbols.rb
@@ -8,6 +8,6 @@ second_string = "second_string"
 
 # Interpolation
 puts "Hello #{first_string} and #{second_string}"
-puts 'Hello #{first_string} and #{second_string}'
-puts "2 + 2 = #{2=2}"
-puts '2 + 2 = #{2=2}'
+puts 'Hello #{first_string} and #{second_string}' # It doesn't evaluate the expression because '' are used
+puts "2 + 2 = #{2+2}"
+puts '2 + 2 = #{2=2}'                             # It doesn't evaluate the expression because '' are used
