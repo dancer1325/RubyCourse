6e9cf877fe93b65e1c3a68f654200eb218d6b9be
dancer1325 <alfredotic0809@gmail.com>
2022-02-20 18:40:29 +0100

    Difference between 2 string variables with same value

--- FirstChapter/StringAndSymbols.rb
+++ FirstChapter/StringAndSymbols.rb
@@ -11,3 +11,10 @@ puts "Hello #{first_string} and #{second_string}"
 puts 'Hello #{first_string} and #{second_string}' # It doesn't evaluate the expression because '' are used
 puts "2 + 2 = #{2+2}"
 puts '2 + 2 = #{2=2}'                             # It doesn't evaluate the expression because '' are used
+
+# Although 2 string variables have got the same value, the object itself is different
+# === Point to different address memory
+hello = "Hello"
+second_hello = "Hello"
+puts hello.object_id
+puts second_hello.object_id
