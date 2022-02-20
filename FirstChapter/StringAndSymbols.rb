5be3a27d67bb46dda04127d2b17b3a681c6e34c3
dancer1325 <alfredotic0809@gmail.com>
2022-02-20 18:08:32 +0100

    Add StringsAndSymbols

---
+++ FirstChapter/StringAndSymbols.rb
@@ -0,0 +1,12 @@
+# Strings
+
+# Declare Strings
+# 1. ''
+first_string = 'first_string'
+# 2. ""
+second_string = "second_string"
+
+# Interpolation
+puts "Hello #{first_string} and #{second_string}"
+puts 'Hello #{first_string} and #{second_string}'
+puts '2 + 2 = #{2=2}'
