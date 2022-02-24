9fce648d7410ab5ae6bb1b11eeb8ffaed977fbe7
dancer1325 <alfredotic0809@gmail.com>
2022-02-24 15:07:57 +0100

    Fix wording error

--- FirstChapter/iteration.rb
+++ FirstChapter/iteration.rb
@@ -51,7 +51,7 @@ puts "Collection after applying map! #{array}"
 # Last expression into the block of code must be a boolean expression
 array = [1, 2, 3, 4]
 puts "Array previously to select operation #{array}"
-a.select do |element|
+array.select do |element|
     puts "element #{element}"
     element > 2
 end
