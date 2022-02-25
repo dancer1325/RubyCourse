7e957cc5e50749e610ae03967d82557ff9222c55
dancer1325 <alfredotic0809@gmail.com>
2022-02-25 21:25:07 +0100

    rescue specific exception

--- SecondChapter/exceptions.rb
+++ SecondChapter/exceptions.rb
@@ -5,6 +5,9 @@ number = gets.to_i
 begin
     result = 10 / number
     puts "10/ #{number} = #{result}"
+rescue ZeroDivisionError                                    # rescue a specific exception
+    puts "ZeroDivisionError! Your number introduced was #{number}"
+    exit
 rescue
     puts "Error! Your number introduced was #{number}"
     exit                                                    # Stop the execution of the rest of the class
