a5deab61001aaf59734bae249bc43438806214e4
dancer1325 <alfredotic0809@gmail.com>
2022-02-25 21:22:10 +0100

    Add exception.rb

---
+++ SecondChapter/exceptions.rb
@@ -0,0 +1,12 @@
+# begin rescue
+puts "Enter a number"
+number = gets.to_i
+
+begin
+    result = 10 / number
+    puts "10/ number = #{result}"
+rescue
+    puts "Error! Your number introduced was #{number}"
+    exit                                                    # Stop the execution of the rest of the class
+end
+
