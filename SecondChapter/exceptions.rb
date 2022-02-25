52704ecd1501df71c86d725e0ce185deb716335e
dancer1325 <alfredotic0809@gmail.com>
2022-02-25 21:59:15 +0100

    Add logging

--- SecondChapter/exceptions.rb
+++ SecondChapter/exceptions.rb
@@ -17,12 +17,12 @@ end
 # def method                     # If you don't pass the variable, it's going to throw an error, since it's another scope
 def method(number)
     result = 10 / number
-    puts "10/ #{number} = #{result}"
+    puts "[method] 10/ #{number} = #{result}"
 rescue ZeroDivisionError                                    # rescue a specific exception
-    puts "ZeroDivisionError! Your number introduced was #{number}"
+    puts "[method] ZeroDivisionError! Your number introduced was #{number}"
     exit
 rescue
-    puts "Error! Your number introduced was #{number}"
+    puts "[method] Error! Your number introduced was #{number}"
     exit                                                    # Stop the execution of the rest of the class
 end
 
