074f9778f63071ffe3f97c4c98a0ff0e999fd136
dancer1325 <alfredotic0809@gmail.com>
2022-02-25 20:31:51 +0100

    Add true and false expressions

---
+++ SecondChapter/true_nil.rb
@@ -0,0 +1,18 @@
+# All expressions return true, except to
+# 1) False expressions
+# 2) nil
+
+def method
+end
+
+def test(*cases, name_of_the_object)
+   case name_of_the_object
+   when :method
+        if cases
+            puts "[Method] is a truth expression"
+        else
+            puts "[Method] is a false expression"
+   else
+        puts "Unknown name_of_the_object #{name_of_the_object}"
+   end
+end
