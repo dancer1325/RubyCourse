c2ec8e6dd41241cf4f74f0f4c11b70b59bd27894
dancer1325 <alfredotic0809@gmail.com>
2022-02-25 21:23:03 +0100

    Modify log

--- SecondChapter/exceptions.rb
+++ SecondChapter/exceptions.rb
@@ -4,7 +4,7 @@ number = gets.to_i
 
 begin
     result = 10 / number
-    puts "10/ number = #{result}"
+    puts "10/ #{number} = #{result}"
 rescue
     puts "Error! Your number introduced was #{number}"
     exit                                                    # Stop the execution of the rest of the class
