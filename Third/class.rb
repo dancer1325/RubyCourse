a8f78ec7e5465759f63334dd31259d084b03225b
dancer1325 <alfredotic0809@gmail.com>
2022-02-26 13:30:08 +0100

    Public, private and protected methods

--- Third/class.rb
+++ Third/class.rb
@@ -1,4 +1,3 @@
-
 class Person
     def initialize(name, surname, age, gender, mood, status)  # Class's constructor
         @name = name                        # Way to specify the class's attribute to the variable
@@ -26,6 +25,28 @@ class Person
     attr_accessor :age, :status             # Create "age", "status" getter methods and "age=()", "status=()" setter methods. Several can be created at the same time
     attr_reader :gender                     # Create "gender" getter method
     attr_writer :mood                       # Create "mood=()" setter method
+
+    def walk                                # By default all methods are public
+        puts "I can walk!"
+    end
+
+    private                                 # All block of code which appears below and into the class, is private. Only available inside the class
+
+    def display_name_age
+        puts "My name is #{@name}, and I am #{@age} years old"
+    end
+
+    def display_name_mood
+        puts "My name is #{@name}, and I feel #{@mood}"
+    end
+
+    protected                               # All block of code which appears below and into the class, is protected. Only available inside the class and children one's
+
+    def is_minor
+        is_minor = @age >= 18
+        puts "I am #{@age} years old, so #{is_minor} minor"
+        is_minor
+    end
 end
 
 # person = Person.new('Alfredo', 'Toledano')  # If we don't pass the right number of arguments, since all are mandatory --> It will throw an error
@@ -51,3 +72,7 @@ puts "person.status #{person.status}"       # Getter method created automaticall
 puts "person.gender #{person.gender}"       # Getter method created automatically by attr_reader
 person.mood = "Passionated"                 # Setter method created automatically by attr_writer
 puts "person.full_name_and_mood #{person.full_name_and_mood}"   # Check the value of mood because it hasn't got a specific getter method
+
+person.walk                                 # Public method --> Accessible outside the class
+#person.display_name_age                     # Private method --> Not accessible outside the class. It will throw an error
+#person.is_minor                             # Protected method --> Not accessible outside the class. It will throw an error
