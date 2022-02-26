210f8621206c1622ebcd22ce470a2b69d5580dc5
dancer1325 <alfredotic0809@gmail.com>
2022-02-26 13:08:11 +0100

    Add attr_accessor, attr_reader and attr_writer

--- Third/class.rb
+++ Third/class.rb
@@ -1,21 +1,53 @@
 
 class Person
-    def initialize(name, surname)           # Class's constructor
+    def initialize(name, surname, age, gender, mood, status)  # Class's constructor
         @name = name                        # Way to specify the class's attribute to the variable
         @surname                            # Necessary to associate attribute with the variable
+        @age = age
+        @gender = gender
+        @mood = mood
+        @status = status
     end
-    def full_name                           # Another class's method
-        "#{@name} #{@surname}"              # Attributes are accessible via @
+    def full_name_and_mood                           # Another class's method
+        "#{@name} #{@surname} with mood #{@mood}"              # Attributes are accessible via @
     end
     def name                                # Getter method
         @name
     end
+    def name_setter(new_name)               # Common setter method
+        @name = new_name
+    end
     def surname                             # Getter method
         @surname
     end
+    def surname=(new_surname)               # Different setter method style
+        @surname = new_surname
+    end
+    attr_accessor :age, :status             # Create "age", "status" getter methods and "age=()", "status=()" setter methods. Several can be created at the same time
+    attr_reader :gender                     # Create "gender" getter method
+    attr_writer :mood                       # Create "mood=()" setter method
 end
 
-person = Person.new('Alfredo', 'Toledano')
-puts "person.full_name #{person.full_name}"
+# person = Person.new('Alfredo', 'Toledano')  # If we don't pass the right number of arguments, since all are mandatory --> It will throw an error
+person = Person.new('Alfredo', 'Toledano', 29, "Male", "Happy", "Single")
+puts "person.full_name_and_mood #{person.full_name_and_mood}" # Surname doesn't appear
+
+puts "person.name #{person.name}"           # Invoke getter method
+puts "person.surname #{person.surname}"     # Invoke getter method. Empty because the constructor created doesn't assign passed variable to the attribute
+puts "person.age #{person.age}"             # Invoke getter method created automatically
+puts "person.status #{person.status}"       # Invoke getter method created automatically
+puts "person.gender #{person.gender}"       # Invoke getter method created automatically
+#puts "person.mood #{person.mood}"           # It throws an error because attr_writer just creates setter method
+
+person.name_setter('Perico')                # Common setter method
 puts "person.name #{person.name}"
+person.surname = 'DeLosPalotes'             # Different setter method
 puts "person.surname #{person.surname}"
+person.age = 30                             # Setter method created automatically by attr_accessor
+puts "person.age #{person.age}"             # Getter method created automatically by attr_accessor
+person.status = "Married"                   # Setter method created automatically by attr_accessor
+puts "person.status #{person.status}"       # Getter method created automatically by attr_accessor
+#person.gender = "Unknown"                   # It throws an error because attr_reader just creates getter method
+puts "person.gender #{person.gender}"       # Getter method created automatically by attr_reader
+person.mood = "Passionated"                 # Setter method created automatically by attr_writer
+puts "person.full_name_and_mood #{person.full_name_and_mood}"   # Check the value of mood because it hasn't got a specific getter method
